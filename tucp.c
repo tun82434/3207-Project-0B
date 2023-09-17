81e136d22cf65698b8ff75f590d4d1803bf3c085
tun82434@temple.edu <tun82434@temple.edu>
2023-09-16 21:09:47 -0400

    Update tucp.c

--- tucp.c
+++ tucp.c
@@ -23,25 +23,13 @@ int main(int argc, char **argv){
 
 int twoARGC(char **argv){
     struct stat path1, path2;
+    FILE *sFile, *dFile;
+    sFile = fopen(argv[1],"r");
+    dFile = fopen(argv[2],"w");
     if (stat(argv[1], &path1) == 0 && stat(argv[2], &path2) == 0){
-        if (S_ISREG(path1.st_mode) && S_ISREG(path2.st_mode)){ // tucp sourceFile destinationFile
-            FILE *sFile, *dFile;
-            sFile = fopen(argv[1],"r");
-            dFile = fopen(argv[2],"w");
-            if (sFile == NULL){ // Source File could not be opened
-                printf("Error: Source File could not be opened.\n");
-                exit(1);
-            }
-            if (dFile == NULL){ // Destination could not be opened
-                printf("Error: Destination File could not be opened.\n");
-                exit(1);
-            }
-            
-        } else if (S_ISREG(path1.st_mode) && S_ISDIR(path2.st_mode)){ // tucp sourceFile destinationDirectory
-            printf("tucp sourceFile destinationDirectory\n");
-        } else{ // Invalid inputs
-            printf("Error: Invalid arguments.\n");
-            exit(1);
+        char line[100];
+        while (fgets(line, sizeof(line), sFile) != NULL){
+            fputs(line, dFile);
         }
     } else{
         printf("Error: Invalid path arguments.\n");
