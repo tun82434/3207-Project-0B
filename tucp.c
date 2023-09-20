a0e315b3006c706971078f3e7f46c1e9daeb3d1c
tun82434@temple.edu <tun82434@temple.edu>
2023-09-20 00:27:29 -0400

    Update tucp.c

--- tucp.c
+++ tucp.c
@@ -23,7 +23,7 @@ int main(int argc, char **argv){
 
 int twoARGC(char **argv){
     struct stat path1, path2;
-    FILE *sFile, *dFile;
+    FILE *sFile, *dFile; 
     if(stat(argv[1], &path1) == 0 && S_ISDIR(path1.st_mode)){ 
         printf("Error: Source file cannot be a directory.\n"); // argv[1] is a directory
         exit(1);
@@ -37,14 +37,23 @@ int twoARGC(char **argv){
         exit(1);
     }
     if (stat(argv[2], &path2) == 0 && S_ISDIR(path2.st_mode)){ // argv[2] is a destination directory
-        char *fileName = strrchr(argv[1], '/') + 1;
+        char *fileName = strrchr(argv[1], '/');
+        if (fileName != NULL){
+            fileName += 1;
+        } else{
+            fileName = argv[1];
+        }
         size_t dPathLen = strlen(argv[2]) + strlen(fileName) + 1;
-        char *dPath = (char *)malloc(dPathLen + 1);
+        char *dPath = (char *)malloc(dPathLen + 1); // allocate space
         strcpy(dPath,argv[2]);
         strcat(dPath, "/");
         strcat(dPath, fileName);
-        dFile = fopen(dPath, "wb");
-        char line[100];
+        dFile = fopen(dPath, "wb"); // dFile open with write binary
+        if(dFile == NULL){
+            printf("Error: Invalid destination file path. fopen() failure.\n");
+            exit(1);
+        }
+        char line[100]; // initalize line for parsing through sFile
         size_t bytes;
         while ((bytes = fread(line, 1, sizeof(line), sFile)) > 0){
             fwrite(line, 1, bytes, dFile);
@@ -54,6 +63,7 @@ int twoARGC(char **argv){
         free(dPath);
         return 0;
     }
+    // if dFile is a file
     dFile = fopen(argv[2], "wb");
     if (dFile == NULL){
         printf("Error: Invalid destination file path. fopen() failure.\n"); // argv[2] is a invalid path
@@ -72,7 +82,7 @@ int twoARGC(char **argv){
 int nARGC(int argc, char **argv){
     int arguments = argc - 1; // Remove the ./tuls argument from the count
     for (int i = 1; i <= arguments; i++){
-        if(i != arguments){
+        if(i != arguments){ // if argv[i] are supposed to be source files
             FILE *sFile, *dFile;
             struct stat file;
             if(stat(argv[i], &file) == 0 && S_ISDIR(file.st_mode)){ // directory
@@ -82,7 +92,12 @@ int nARGC(int argc, char **argv){
                 printf("Error: Argument [%d] is an invalid source file path.\n", i);
                 exit(1);
             }
-            char *fileName = strrchr(argv[i], '/') + 1;
+            char *fileName = strrchr(argv[i], '/');
+            if (fileName != NULL){
+                fileName += 1;
+            } else{
+                fileName = argv[i];
+            }
             size_t pathLen = strlen(argv[arguments]) + strlen(fileName) + 1;
             char *dPath = (char *)malloc(pathLen + 1);
             strcpy(dPath, argv[arguments]);
